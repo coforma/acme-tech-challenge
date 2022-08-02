ff0eba82f29df55bc3d01dc4236266d0e66755e3
saree <saree@LAPTOP-YOGA-2020>
2022-08-02 16:32:42 -0400

    form based spring security changes

--- acme/src/main/java/com/acme/config/WebSecurityConfig.java
+++ acme/src/main/java/com/acme/config/WebSecurityConfig.java
@@ -2,22 +2,32 @@ package com.acme.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import com.acme.common.AppUserDetailsService;
 
 @Configuration
-public class WebSecurityConfig { //extends WebSecurityConfigurerAdapter
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//    }
+public class WebSecurityConfig { 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http, AppUserDetailsService appUserDetailsService) throws Exception {
+    	AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    	authenticationManagerBuilder.userDetailsService(appUserDetailsService);
+    	
+    	http.authorizeHttpRequests((authz) -> authz.antMatchers("/","index.html","/login","/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll().anyRequest().authenticated()).formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .defaultSuccessUrl("/swagger-ui/index.html", true).and()
+        .authenticationManager(authenticationManagerBuilder.build());
+        
         return http.csrf().disable().build();
     }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+    };
 }
