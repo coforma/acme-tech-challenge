c9ab1ffafce6fadc34794e94d231f5a3e02e59cb
caoabunga <caoabunga@me.com>
2022-08-02 22:13:53 -0700

    Adding lombok

--- acme/src/main/java/com/acme/event/Event.java
+++ acme/src/main/java/com/acme/event/Event.java
@@ -1,10 +1,12 @@
 package com.acme.event;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Data
 @Entity
 public class Event {
     @Id
@@ -19,19 +21,6 @@ public class Event {
     public Event(long id, String name) {
         super();
         this.id = id;
-        this.name = name;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
