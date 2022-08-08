9c15011b5ef1a13cebc1d208baefea61186cce6e
Yonas <yonas@samtek-inc.com>
2022-08-08 16:18:27 -0400

    update CIDR for prod

--- setup/variables.tf
+++ setup/variables.tf
@@ -1,9 +1,9 @@
 variable "region" { default = "us-east-1" }
 variable "project" { default = "acme-challenge" }
-variable "environment" { default = "impl" }
+variable "environment" { default = "prod" }
 variable "team" { default = "coforma" }
 
-variable "vpc_cidr" { default = "10.3.0.0/16"} #TODO: Update default
+variable "vpc_cidr" { default = "10.4.0.0/16"}
 variable "vpc_azs" {  
   type        = list(any)
   default     =  ["us-east-1a", "us-east-1b"]  
@@ -11,12 +11,12 @@ variable "vpc_azs" {
 
 variable "public_subnets" {  
   type        = list(any)
-  default     =  ["10.3.101.0/24", "10.3.102.0/24"]   #TODO: Update default
+  default     =  ["10.4.101.0/24", "10.4.102.0/24"]  
 }
 
 variable "private_subnets" {  
   type        = list(any)
-  default     =  ["10.3.103.0/24", "10.3.104.0/24"]   #TODO: Update default
+  default     =  ["10.4.103.0/24", "10.4.104.0/24"]   
 }
 
 variable "db_password" {
