767769794670567f5a2e28618db267dcb57e9a02
Dan Miller <dan@coforma.io>
2022-08-10 13:56:29 -0400

    COMP: Updates codeowners, fixes bug with facility data being returned

--- acme/src/main/java/com/acme/controller/PatientDisasterStatusController.java
+++ acme/src/main/java/com/acme/controller/PatientDisasterStatusController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,9 +74,9 @@ public class PatientDisasterStatusController {
 	public GetPatientDisasterStatusOutput getPatientDisasterStatus(@RequestParam(required=true) Long facilityNpi , @RequestParam(required=true) String patientIdFromFacility 
 			, Authentication authentication) {
 		
-		
-		checkPermissions(facilityNpi, authentication);
-	
+		if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("EHR"))) {
+			checkPermissions(facilityNpi, authentication);
+		}
 		return patientDisasterStatusService.getPatientStatus(facilityNpi, patientIdFromFacility);
 	}
 	
